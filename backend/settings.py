4d58976c45221fd367ffef10164866145561a005
kollydap <dapkolly@gmail.com>
2025-03-03 15:51:52 +0100

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -18,7 +18,7 @@ DEBUG = os.getenv("DEBUG", True)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
