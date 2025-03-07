29f1c4db53e9f98fbc031d4358ef33aa08db3913
kollydap <dapkolly@gmail.com>
2025-03-07 02:13:00 +0100

    modified asgi to properly setup django before importing any module that uses models

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,11 +1,15 @@
 import os
-
+import django
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
+django.setup()
+
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 import core.routing
 
 application = ProtocolTypeRouter(
